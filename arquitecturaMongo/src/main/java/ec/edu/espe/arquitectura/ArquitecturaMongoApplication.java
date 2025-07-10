4c7a22908e24d4c2c2f4f6fef98895405b54b2ce
dftorresg22 <105184551+10038-dftorres8@users.noreply.github.com>
2025-07-09 19:40:43 -0500

    V0.1

---
+++ arquitecturaMongo/src/main/java/ec/edu/espe/arquitectura/ArquitecturaMongoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaMongoApplication.class, args);
+	}
+
+}
---
+++ arquitecturaMongo/src/main/java/ec/edu/espe/arquitectura/exception/DeleteEntityException.java
@@ -0,0 +1,20 @@
+package ec.edu.espe.arquitectura.exception;
+
+public class DeleteEntityException extends RuntimeException {
+
+    private final Integer errorCode;
+
+    public DeleteEntityException(String entityName, String message) {
+        super(entityName + ": " + message);
+        this.errorCode = 500;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error code: " + this.errorCode + ", message: " + super.getMessage();
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+}
